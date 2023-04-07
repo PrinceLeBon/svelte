4dc936089d025ec6d513da3ae7396eba91da4e2e
Orphé Houessinon <orphe.h00@gmail.com>
2023-04-07 14:57:41 +0100

    add bootstrap an custom displaying

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,34 +53,82 @@
 	};
 </script>
 
-<h1>Ajouter une tâche</h1>
+<div class="row justify-content-between ">
+	<div class="col-md-4 mt-4">
+		<h2>Ajouter une tâche</h2>
 
-<form on:submit={handleSubmit}>
-	<label>
-		Titre de la tâche :
-		<input type="text" bind:value={title} />
-	</label>
-	<label>
-		Description de la tâche :
-		<input type="text" bind:value={description} />
-	</label>
-	<button type="submit">Ajouter</button>
-</form>
+		<div class="card row mt-4 pt-2 shadow">
+			<div class="card-body">
+				<form on:submit={handleSubmit} class="">
+					<div class="row align-items-center">
+						<label class="col-md-4 form-label">Titre de la tâche</label>
+						<div class="col-md-8">
+							<input type="text" bind:value={title} class="form-control form-control-sm"/>
+						</div>
+					</div>
+					<div class="row mt-2 align-items-center">
+						<label class="col-md-4">Description de la tâche</label>
+						<div class="col-md-8">
+							<input type="text" bind:value={description} class="form-control form-control-sm"/>
+						</div>
+					</div>
+					<div class="d-flex justify-content-end">
+						<button type="submit" class="btn btn-sm btn-primary mt-3">Ajouter</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-7 mt-4">
+		<div class="row justify-content-end">
+			<h2 class="mb-2">Liste des tâches</h2>
 
-<h1>Liste des tâches</h1>
+			<div class="col-md-12 card shadow mt-2 pt-2">
+				<div class="card-body">
 
-<ul>
-	{#each tasks as task (task.id)}
-		<li>
-			<input
-				type="checkbox"
-				bind:checked={task.completed}
-				on:change={() => handleUpdateTask(task)}
-			/>
-			<input type="text" bind:value={task.title} />
-			<input type="text" bind:value={task.description} />
-			<button on:click={() => handleUpdateTask(task)}>Modifier</button>
-			<button on:click={() => handleDeleteTask(task)}>Supprimer</button>
-		</li>
-	{/each}
-</ul>
+					<table class="table table-sm">
+						<tbody>
+							<tr class="text-uppercase fs-7">
+								<th scope="col"></th>
+								<th scope="col">Titre de la tâche</th>
+								<th scope="col">Description</th>
+								<th scope="col">Actions</th>
+							</tr>
+						  {#each tasks as task (task.id)}
+							<tr class="">
+								<td class="">
+									<div class="form-check">
+										<input class="form-check-input"
+											type="checkbox"
+											bind:checked={task.completed}
+											on:change={() => handleUpdateTask(task)}
+											/>
+									</div>
+								</td>
+								<td>
+									<input type="text" bind:value={task.title} class="form-control form-control-sm" />
+								</td>
+								<td>
+									<input type="text" bind:value={task.description} class="form-control form-control-sm"/>
+								</td>
+								<td class="d-flex d-inline">
+									<button class="btn btn-sm btn-outline-primary" on:click={() => handleUpdateTask(task)}><i class="fa fa-edit text-"></i></button>
+									<button class="btn btn-sm btn-outline-danger mx-1" on:click={() => handleDeleteTask(task)}><i class="fa fa-trash "></i></button>
+								</td>
+							</tr>
+						{/each}
+						</tbody>
+					</table>
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
